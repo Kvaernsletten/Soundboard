f3fef7b2e0338c9c88742ab42deca6a261fbfe17
Kvaernsletten <kvaernsletten@gmail.com>
2024-02-09 10:49:57 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// let cowAudio = new Audio("sfx/Cow.wav");
+// let sheepAudio = new Audio("sfx/Sheep.wav");
+// let dogAudio = new Audio("sfx/Dog.mp3");
+// let lionAudio = new Audio("sfx/Lion.wav");
+// let catAudio = new Audio("sfx/Cat.wav");
+// let elephantAudio = new Audio("sfx/Elephant.wav");
+
+
+function playAudio(audio){
+    audio.play()
+}
